005fafe0f45e3d0a1bbd942415acd5d0fa78b393
WouterDS <wouter.de.schuyter@gmail.com>
2015-09-29 13:11:16 +0200

    Added check to install script to determine if Atom is installed correctly

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-echo "Installing atom packages"
+echo "Checking if Atom installed.."
+if !type "apmm" >/dev/null 2>/dev/null; then
+	echo "Please install Atom first!"
+	open "https://atom.io/"
+	exit 0
+fi
+echo "OK"
+
+echo "Installing Atom packages"
 apm install `cat atom-packages.list`
